40083dbbec1859d893ed7e71b11d0edca02c63de
yeranik <136292592+yeranik@users.noreply.github.com>
2023-11-16 22:48:58 -0600

    added html to react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="weather-app">
+        <div class="row">
+          <div class="col-4">
+            <h1>New York</h1>
+            <li>
+              <span id="date">Friday 05:00</span>,
+              <span class="weatherDescription" id="des">
+                clear sky
+              </span>
+            </li>
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+              alt=""
+              id="icon-Main"
+              class="iconCanvas"
+            />
+            <div class="d-inline-flex">
+              <div class="temperature" id="temp">
+                28
+              </div>
+              <span class="units">
+                <a href="#" id="celsius-Link" class="active">
+                  C°
+                </a>
+                |
+                <a href="#" id="farenheit-link">
+                  F°
+                </a>
+              </span>
+            </div>
+            <div class="weather-information">
+              <li>
+                Humidity: <strong id="humidity-Element">85</strong>
+                <strong> %</strong>, Wind:
+                <strong class="windSpeed" id="wind-Speed">
+                  2.7
+                </strong>
+                <strong> km/h</strong>
+              </li>
+            </div>
+            <div class="row">
+              <form id="search-form">
+                <div class="d-flex justify-content-center">
+                  <div class="d-inline-flex p-2">
+                    <input
+                      class="form-control form-control-sm"
+                      type="text"
+                      autocomplete="off"
+                      placeholder="Enter city"
+                      id="enter-City"
+                    />
+
+                    <input
+                      class="btn btn-primary btn-sm"
+                      type="submit"
+                      value="Search"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-8" id="forecast"></div>
+        </div>
+        <br />
+        <small>
+          This project was coded by
+          <a
+            class="Instagram"
+            href="https://www.instagram.com/yerani_k/?hl=es-la"
+            target="_blank"
+          >
+            Yerani Kingston
+          </a>
+          and is
+          <a
+            class="GithubLink"
+            href="https://github.com/yeranik/vw-app"
+            target="_blank"
+          >
+            Open-source code on Github
+          </a>
+          and
+          <a
+            class="netlify"
+            href="https://precious-dragon-ab7660.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
-
-export default App;
